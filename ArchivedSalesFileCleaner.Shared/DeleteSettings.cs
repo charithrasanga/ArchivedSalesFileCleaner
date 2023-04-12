300305f2b767e35a9260099a5dfb80da221ba81d
Charith Wickramasinghe <charithrasanga@gmail.com>
2023-04-12 08:20:32 +0530

    Added Proper Project structure and now using DI to Inject IConfiguration to services

---
+++ ArchivedSalesFileCleaner.Shared/DeleteSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace ArchivedSalesFileCleaner.Shared
+{
+    public class DeleteSettings
+    {
+        public int RetentionPeriodInDays { get; set; }
+        public string SourceDirectory { get; set; }
+        public bool RemoveFilePhysically { get; set; }
+        public string ArchivePath { get; set; }
+    }
+}
---
+++ ArchivedSalesFileCleaner.Shared/FileOperationStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace ArchivedSalesFileCleaner.Shared
+{
+    public enum FileOperationStatus
+    {
+        Delete,
+        Archive
+    }
+}
